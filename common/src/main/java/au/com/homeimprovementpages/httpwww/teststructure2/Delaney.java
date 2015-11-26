fd422fb95021a0a214d785466eaf4668bcde06d6
Delaney <stimsoni9@gmail.com>
2015-11-27 08:35:04 +1100

    testing as submodule

---
+++ common/src/main/java/au/com/homeimprovementpages/httpwww/teststructure2/Delaney.java
@@ -0,0 +1,8 @@
+package au.com.homeimprovementpages.httpwww.teststructure2;
+
+/**
+ * Created by michaeldelaney on 27/11/15.
+ */
+public class Delaney {
+    public static String test = "Tada";
+}
